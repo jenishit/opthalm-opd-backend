ebc815da86631848a2e3657289ff64c83e64a433
Jenish Shrestha <jneece.cresta@gmail.com>
2026-07-16 17:53:02 +0545

    Feat: Patient repo added

--- internal/adapter/storage/postgres/migrations/20260716072301_create_patient.sql
+++ internal/adapter/storage/postgres/migrations/20260716072301_create_patient.sql
@@ -1,5 +1,20 @@
 -- +goose Up
-SELECT 'up SQL query';
+CREATE TABLE patients (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    full_name VARCHAR(100) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
+    address VARCHAR(255),
+    dob DATE NOT NULL,
+    gender VARCHAR(10) NOT NULL DEFAULT 'other'
+                     CHECK (gender IN ('male','female','other')),
+    occupation VARCHAR(100),
+    registered_on DATE NOT NULL,
+    created_by UUID NOT NULL REFERENCES users(id),
+    deleted_at TIMESTAMP DEFAULT NULL,
+    updated_by UUID NOT NULL REFERENCES users(id),
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP NOT NULL DEFAULT now()
+);
 
 -- +goose Down
-SELECT 'down SQL query';
+DROP TABLE patients;
